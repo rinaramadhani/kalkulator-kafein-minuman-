a5695df61b0aa94cd6b3baef267d86ab4f8e1943
rinaramadhani <rinaramadhani910@gmail.com>
2025-05-17 18:11:30 +0700

    Update kalkulator minuman.py

--- kalkulator minuman.py
+++ kalkulator minuman.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.markdown("""
     <style>
     .stApp {
-        background-image: url("https://i.pinimg.com/736x/9b/e8/32/9be83278b48a97b9218b58f236c2be28.jpg");
+        background-image: url("https://i.pinimg.com/736x/9b/e8/32/9be83278b48a97b9218b58f236c2be28.jpg")
         background-size: cover;
         background-attachment: fixed;
         background-repeat: no-repeat;
